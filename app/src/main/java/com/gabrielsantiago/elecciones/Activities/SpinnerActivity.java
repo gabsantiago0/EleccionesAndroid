c76ed58c1ac82c0483bb96773aa4266535f2ebdd
gabsantiago0 <gabsantiagoalfonso@gmail.com>
2023-11-12 22:09:28 +0100

    Commit 3

--- app/src/main/java/com/gabrielsantiago/elecciones/Activities/SpinnerActivity.java
+++ app/src/main/java/com/gabrielsantiago/elecciones/Activities/SpinnerActivity.java
@@ -3,13 +3,20 @@ package com.gabrielsantiago.elecciones.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.gabrielsantiago.elecciones.Adapter.spinnerAdapter;
 import com.gabrielsantiago.elecciones.R;
 
+import java.util.ArrayList;
+
 public class SpinnerActivity extends AppCompatActivity {
 
+    public final int MAX_VOTOS = 3;
     String[] listaNombres = {
             "Doña Concha","Marisa","Vicenta",
             "Lucía","Belén", "Bea",
@@ -35,6 +42,7 @@ public class SpinnerActivity extends AppCompatActivity {
     };
 
     Spinner spinnerCandidatos;
+    Button botonVotos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +52,44 @@ public class SpinnerActivity extends AppCompatActivity {
         spinnerCandidatos = findViewById(R.id.spinnerElecciones);
         spinnerAdapter adapter = new spinnerAdapter(getApplicationContext(),listaNombres,listaFotos,listaPartidos);
         spinnerCandidatos.setAdapter(adapter);
+
+        botonVotos= findViewById(R.id.buttonVotos);
+
+        ArrayList<String>votos = new ArrayList<>();
+        spinnerCandidatos.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (position==1){
+                    String seleccion = parent.getSelectedItem().toString();
+                    votos.add(seleccion);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
+        botonVotos.setOnClickListener(new View.OnClickListener() {
+            int contador = 0;
+            @Override
+            public void onClick(View v) {
+                contador++;
+                ArrayList<String> votos = new ArrayList<>();
+                    if (contador==0){
+                        botonVotos.setText("Votos 0/3");
+                    } else if (contador==1){
+                        botonVotos.setText("Votos 1/3");
+                    } else if (contador==2) {
+                        botonVotos.setText("Votos 2/3");
+                    } else if (contador==3) {
+                        botonVotos.setEnabled(false);
+                        botonVotos.setText("Votos 3/3");
+                    }
+                }
+        });
 
     }
 }
